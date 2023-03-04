6886265a47a4910beccfdd00884a8dd3fd2630d4
Shreyas Bhat K <67960703+Arkanobot@users.noreply.github.com>
2023-03-04 18:31:15 +0530

    implimented a Static Chart with values from Api

--- src/components/Dashboard/Chart.jsx
+++ src/components/Dashboard/Chart.jsx
@@ -30,10 +30,11 @@ export default function Chart() {
   const { currency } = useSelector((store) => store.currency);
   const [coinData, setCoinData] = useState();
   const crypto = "bitcoin";
+  const duration = 365;
   async function fetchData() {
     try {
       const response = await axios.get(
-        `https://api.coingecko.com/api/v3/coins/${crypto}/market_chart?vs_currency=${currency}&days=365&interval=bi-weekly`
+        `https://api.coingecko.com/api/v3/coins/${crypto}/market_chart?vs_currency=${currency}&days=${duration}&interval=weekly`
       );
       setCoinData(response.data);
     } catch (error) {
@@ -42,11 +43,8 @@ export default function Chart() {
     }
   }
   useEffect(() => {
-    const timer = setTimeout(() => {
-      //function to call axios to get the Crypto data.
-      fetchData();
-    });
-    return () => clearTimeout(timer);
+    //function to call axios to get the Crypto data.
+    fetchData();
   }, [currency]);
   let options;
   let data;
@@ -57,26 +55,43 @@ export default function Chart() {
       y: data[1].toFixed(2),
     }));
     options = {
-      responsive: true,
       maintainAspectRatio: false,
+      plugins: {
+        legend: {
+          align: "end",
+          title: {
+            position: "end",
+          },
+        },
+      },
     };
     data = {
-      labels: coinPrice.map((value) => moment(value.x).format("DD MMM YY")),
+      labels: coinPrice.map((value) => moment(value.x).format("MMM Do YY")),
       datasets: [
         {
           fill: true,
           label: crypto.toUpperCase(),
           data: coinPrice.map((value) => value.y),
+          borderWidth: 2,
           borderColor: "rgb(53, 152, 235)",
-          backgroundColor: "rgba(53,152, 235, 0.2",
+          backgroundColor: "rgba(53,152, 235, 0.2)",
+          hoverBackgroundColor: "rgba(255,99,132,0.4)",
+          hoverBorderColor: "rgba(255,99,132,1)",
         },
       ],
     };
   }
   return (
-    <div className=" my-5 w-[98.5%] h-[100%] overflow-hidden border-2 border-solid border-slate-50 rounded-lg shadow-md bg-white">
+    <div className="flex relative my-5 w-[85vw] h-[35vh] xl:w-[66vw] 2xl:w-[69vw] bg-white">
       {coinData !== undefined ? (
-        <Line option={options} data={data} height={"80%"} />
+        <div style={{ padding: "5px", width: "100%", height: "100%" }}>
+          <Line
+            options={options}
+            data={data}
+            height={"80%"}
+            updateMode="resize"
+          />
+        </div>
       ) : (
         <div>Loading...</div>
       )}
